56e8f00ce5ad174f0ab048cff10b1993d95702af
João Gonzalez <joao@midia.mobi>
2018-05-09 00:32:46 -0300

    v1

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require ('express');
 var app = express();
 
+app.set ('view engine', 'ejs');
+
+
 app.get('/', function (req, res){
 	res.send("<html><body> Portal de notícias <body></html>")
 
